cefc782e7532a7433ba7327ed9a7ad62b731a55c
Lukas Zorich <lukas.zorich@gmail.com>
2020-12-08 15:31:16 -0300

    Ignore error if locale not set

--- fintoc/__init__.py
+++ fintoc/__init__.py
@@ -8,6 +8,10 @@ Let's initialize this machine.
 __version__ = "0.2.1"
 
 import locale
+
 from fintoc.client import Client
 
-locale.setlocale(locale.LC_ALL, "")
+try:
+    locale.setlocale(locale.LC_ALL, "")
+except locale.Error:
+    pass
