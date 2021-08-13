4b80c43d5a38931a0164e507cb69e055bdc6d956
Daniel Leal <dlleal@uc.cl>
2021-08-13 10:39:22 -0400

    feat: remember attributes on the resources

--- fintoc/mixins/resource_mixin.py
+++ fintoc/mixins/resource_mixin.py
@@ -18,17 +18,21 @@ class ResourceMixin(metaclass=ABCMeta):
         self._handlers = handlers
         self._methods = methods
         self._path = path
+        self._attributes = []
 
         for key, value in kwargs.items():
-            resource = self.__class__.mappings.get(key, key)
-            if isinstance(value, list):
-                resource = singularize(resource)
-                element = {} if not value else value[0]
-                klass = get_resource_class(resource, value=element)
-                setattr(self, key, [objetize(klass, client, x) for x in value])
-            else:
-                klass = get_resource_class(resource, value=value)
-                setattr(self, key, objetize(klass, client, value))
+            try:
+                resource = self.__class__.mappings.get(key, key)
+                if isinstance(value, list):
+                    resource = singularize(resource)
+                    element = {} if not value else value[0]
+                    klass = get_resource_class(resource, value=element)
+                    setattr(self, key, [objetize(klass, client, x) for x in value])
+                else:
+                    klass = get_resource_class(resource, value=value)
+                    setattr(self, key, objetize(klass, client, value))
+            except NameError:
+                pass
 
     def __getattr__(self, attr):
         if attr not in self._methods:
