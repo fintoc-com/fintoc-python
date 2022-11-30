f4f6740eb8254f06990c4f9273b7300c98e7e94f
Nicolás Pablo Castro Leal <npcastrol@gmail.com>
2022-11-30 12:58:46 -0300

    fixup! feat: Add charges manager

--- fintoc/managers/charges_manager.py
+++ fintoc/managers/charges_manager.py
@@ -1,10 +1,10 @@
-
 """Module to hold the charges manager."""
 
 from fintoc.mixins import ManagerMixin
 
 
 class ChargesManager(ManagerMixin):
+
     """Represents a charges manager."""
 
     resource = "charge"
