705390104d5800df55ff62a6b231e274a160661b
Francisco Cumsille <jfcumsillei@gmail.com>
2024-04-09 14:55:52 -0400

    fixup! tests(version): add tests to fintoc version header

--- tests/test_core.py
+++ tests/test_core.py
@@ -7,8 +7,20 @@ class TestCoreFintocObject:
     def test_object_creations(self):
         # pylint: disable=protected-access
         api_key = "super_secret_api_key"
-        api_version = "2023-01-01"
-        fintoc = Fintoc(api_key, api_version)
+        fintoc = Fintoc(api_key)
         assert isinstance(fintoc._client, Client)
         assert isinstance(fintoc.links, ManagerMixin)
         assert isinstance(fintoc.webhook_endpoints, ManagerMixin)
+
+    def test_fintoc_creation_with_api_version(self):
+        # pylint: disable=protected-access
+        api_key = "super_secret_api_key"
+        api_version = "2023-01-01"
+        fintoc = Fintoc(api_key, api_version)
+        assert fintoc._client.headers["Fintoc-Version"] == api_version
+
+    def test_fintoc_creation_without_api_version(self):
+        # pylint: disable=protected-access
+        api_key = "super_secret_api_key"
+        fintoc = Fintoc(api_key)
+        assert "Fintoc-Version" not in fintoc._client.headers
