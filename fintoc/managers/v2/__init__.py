fd6ceb51d11f3ad89d0ff5a018478eba4b8f481c
Orlando Andrade <orlando@fintoc.com>
2025-05-23 10:42:49 -0400

    fix(linter): linter issues

--- fintoc/managers/v2/__init__.py
+++ fintoc/managers/v2/__init__.py
@@ -1,8 +1,8 @@
 """Init file for the v2 managers module of the SDK."""
 
 from .account_numbers_manager import AccountNumbersManager
-from .accounts_manager import AccountsManager
 from .account_verifications_manager import AccountVerificationsManager
+from .accounts_manager import AccountsManager
 from .entities_manager import EntitiesManager
 from .simulate_manager import SimulateManager
 from .transfers_manager import TransfersManager
