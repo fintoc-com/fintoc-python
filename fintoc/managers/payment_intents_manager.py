413ca29340766b432e976010d4a84f4607c44a63
Fenho <foarriagada@uc.cl>
2025-07-09 18:10:12 -0400

    feat(pi): add expires functionality

--- fintoc/managers/payment_intents_manager.py
+++ fintoc/managers/payment_intents_manager.py
@@ -8,4 +8,9 @@ class PaymentIntentsManager(ManagerMixin):
     """Represents a payment_intents manager."""
 
     resource = "payment_intent"
-    methods = ["list", "get", "create"]
+    methods = ["list", "get", "create", "expire"]
+
+    def _expire(self, identifier, **kwargs):
+        """Expire a payment intent."""
+        path = f"{self._build_path(**kwargs)}/{identifier}/expire"
+        return self._create(path_=path, **kwargs)
