caef903660f374902860bd8f09255eeb89314c63
Diego Acevedo <diego@fintoc.com>
2025-07-29 17:14:31 -0400

    feat(movements-resource-v2): add resource

---
+++ fintoc/resources/v2/movement.py
@@ -0,0 +1,8 @@
+# pylint: disable=duplicate-code
+"""Module to hold the Movement resource."""
+
+from fintoc.mixins import ResourceMixin
+
+
+class Movement(ResourceMixin):
+    """Represents a Fintoc Movement."""
