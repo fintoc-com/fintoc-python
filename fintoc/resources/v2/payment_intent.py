edaf5a5433f4cb74ff69b5e61ef6d9c798d0e0ab
Gonzalo Caballero <gonzalo.caballero@fintoc.com>
2026-02-19 10:10:57 -0300

    feat(payment-intent-v2): add resource

---
+++ fintoc/resources/v2/payment_intent.py
@@ -0,0 +1,8 @@
+# pylint: disable=duplicate-code
+"""Module to hold the PaymentIntent resource."""
+
+from fintoc.mixins import ResourceMixin
+
+
+class PaymentIntent(ResourceMixin):
+    """Represents a Fintoc Payment Intent."""
