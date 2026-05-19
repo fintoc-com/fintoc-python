339dabec2c6dd68530e4c03dc99957f1bae5c97e
mamunozcar <m.munozcarr@gmail.com>
2026-05-19 11:45:06 -0400

    feat: add support for disputes endpoints

---
+++ fintoc/resources/dispute.py
@@ -0,0 +1,12 @@
+"""Module to hold the Dispute resource."""
+
+from fintoc.mixins import ResourceMixin
+
+
+class Dispute(ResourceMixin):
+
+    """Represents a Fintoc Dispute."""
+
+    mappings = {
+        "documents": "dispute_document",
+    }
---
+++ fintoc/resources/dispute_document.py
@@ -0,0 +1,7 @@
+"""Module to hold the DisputeDocument resource."""
+
+from fintoc.mixins import ResourceMixin
+
+
+class DisputeDocument(ResourceMixin):
+    """Represents a Fintoc Dispute Document."""
